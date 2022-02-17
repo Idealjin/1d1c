4aac32dabd8c97c6959159ac26cfaa02e5dc4523
Idealjin <99496369+Idealjin@users.noreply.github.com>
2022-02-18 01:34:15 +0900

    Add files via upload
    2022-02-17 팀프로젝트-간단한메모만들기

---
+++ miracle-jdbc-mvc-practice2/src/com/miracle/controller/MnoteController.java
@@ -0,0 +1,97 @@
+package com.miracle.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.miracle.model.dto.MnoteDTO;
+import com.miracle.model.service.MnoteService;
+import com.miracle.view.ResultView;
+
+public class MnoteController {
+
+
+	MnoteService mnoteService = new MnoteService();
+	Scanner sc = new Scanner(System.in);
+	ResultView resultView = new ResultView();
+	
+	
+	
+	public void newNote() {
+		
+		MnoteDTO mnoteDTO = new MnoteDTO();
+		
+		System.out.print("노트 제목을 입력해주세요 : ");
+		String title = sc.nextLine();
+		
+		System.out.print("노트 내용을 입력해주세요 : ");
+		String content = sc.nextLine();
+	 	
+		java.util.Date mnoteTime = new java.util.Date(System.currentTimeMillis());
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yy/MM/dd");
+		String date = dateFormat.format(mnoteTime);
+		
+		mnoteDTO.setTitle(title);
+		mnoteDTO.setContent(content);
+		mnoteDTO.setDate(date);
+		
+		int result = mnoteService.newNote(mnoteDTO);
+		
+		if(result > 0) {
+			resultView.Success();
+		} else {
+			resultView.failed();
+		}
+	}
+
+	public void selNote() {
+		
+		List<MnoteDTO> mnoteDTO = new ArrayList<>();
+		
+		mnoteDTO = mnoteService.selNote();
+		
+		for(MnoteDTO mnoteDTO1 : mnoteDTO) {
+			System.out.println(mnoteDTO1);
+		}
+		
+	}
+
+	public void updateNote() {
+		
+		MnoteDTO mnoteDTO = new MnoteDTO();
+		
+		System.out.print("수정할 노트 번호를 입력해주세요 : ");
+		int selNo = sc.nextInt();
+		
+		mnoteService.selNoteNo(selNo);
+		mnoteDTO.setMemoNo(selNo);
+		
+		sc.nextLine();
+		
+		System.out.print("수정할 제목을 입력해주세요 : ");
+		String title = sc.nextLine();
+		
+		System.out.print("수정할 내용을 입력해주세요 : ");
+		String content = sc.nextLine();
+		
+		
+		mnoteDTO.setTitle(title);
+		mnoteDTO.setContent(content);
+		
+		
+		int result = 0;
+		result = mnoteService.updateNote(mnoteDTO);
+		
+		if(result > 0) {
+			resultView.updateSuccess();
+		} else {
+			resultView.updatefailed();
+		}
+	}
+
+	
+	
+	
+	
+}
---
+++ miracle-jdbc-mvc-practice2/src/com/miracle/view/ResultView.java
@@ -0,0 +1,22 @@
+package com.miracle.view;
+
+public class ResultView {
+
+	public void Success() {
+		System.out.println("======================== 노트 등록 성공!! ========================");
+	}
+	
+	public void failed() {
+		System.out.println("======================== 노트 등록 실패!! ========================");
+	}
+	
+	public void updateSuccess() {
+		System.out.println("======================== 노트 수정 성공!! ========================");
+	}
+	
+	public void updatefailed() {
+		System.out.println("======================== 노트 수정 실패!! ========================");
+	}
+	
+	
+}
